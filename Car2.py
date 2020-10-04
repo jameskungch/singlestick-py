558f14eff3d504b61bc06b5a8b0726a0ed14be12
James Kung <james@matoha.com>
2020-10-04 15:01:13 +0100

    Preparing to move the sprite movement code into the class

--- Car2.py
+++ Car2.py
@@ -22,7 +22,13 @@ clock = pygame.time.Clock()
 
 
 class sprite():
-    def __init__(self, image, x=0, y=0, vx_max=20, vy_max=20, img_width=0, img_height=0, bounce = True, accel = 0.2, drag = 0):
+    def __init__(self, spritetype, image, x=0, y=0, vx_max=20, vy_max=20,
+                 img_width=0, img_height=0, bounce=True, accel=0.2, drag=0,
+                 deadtimeseparation=50):
+        if spritetype in ["player", "ball", "enemy"]:
+            self.spritetype = spritetype
+        else:
+            raise ValueError("spritetype must be one of 'player', 'ball', 'enemy'!")
         self.img = pygame.image.load(image)
         self.x = x
         self.y = y
@@ -38,6 +44,7 @@ class sprite():
         self.accel = accel
         self.drag = drag
         self.deadtime = 50
+        self.deadtimeseparation = deadtimeseparation
 
     def draw(self, gameDisplay):
         gameDisplay.blit(self.img, (self.x, self.y))
@@ -58,29 +65,34 @@ class sprite():
 
     def istouching(self, target):
         xdiff, ydiff = self.distanceto(target, normed=False)
-        if abs(xdiff) < (self.img_width + target.img_width)/2 and abs(ydiff) < (self.img_height + target.img_width)/2:
+        if (abs(xdiff) < (self.img_width + target.img_width)/2 and
+            abs(ydiff) < (self.img_height + target.img_width)/2):
             return True
 
+    # def spritecontrol(self):
+    #     if self.spritetype == "player":
+
+
     def move(self, display_width, display_height):
-        # if self.drag != 0:
-        #     self.ax *= self.drag
-        #     self.ay *= self.drag
-            #magnitude = math.sqrt(self.ax * self.ax + self.ay * self.ay)
         self.vx += self.ax
         if self.vx > self.vx_max:
             self.vx = self.vx_max
         if self.vx < -self.vx_max:
             self.vx = -self.vx_max
+
         self.vy += self.ay
         if self.vy > self.vy_max:
             self.vy = self.vy_max
         if self.vy < -self.vy_max:
             self.vy = -self.vy_max
+
         if self.drag != 0:
             self.vx *= self.drag
             self.vy *= self.drag
+
         self.x += self.vx
         self.y += self.vy
+
         if self.bounce:
             if self.x >= display_width - self.img_width:
                 self.vx = -self.vx
@@ -102,9 +114,10 @@ class sprite():
 
 
 def game_loop():
-    car = sprite("racecar.png", 640, 400, 5, 5, 73, 82, True, 0.2, 0.995)
-    ball = sprite("target.png", 640, 360, 20, 20, 50, 50, True, 0, 0.99)
-    enemy = sprite("redsquare.png", 50, 50, 1, 1, 30,30,True, 0, 0)
+    car = sprite("player", "racecar.png", 640, 400, 5, 5, 73, 82, True, 0.2, 0.995)
+    ball = sprite("ball", "target.png", 640, 360, 20, 20, 50, 50, True, 0, 0.99)
+    enemy = sprite("enemy", "redsquare.png", 50, 50, 1, 1, 30,30,True, 0, 0)
+    enemy2 = sprite("enemy", "redsquare.png", 500, 500, 1, 1, 30,30,True, 0, 0)
     pygame.key.set_repeat(1,1)
     gameExit = False
     while not gameExit:
@@ -121,15 +134,18 @@ def game_loop():
                 car.ay = -car.accel
             if (pygame.key.get_pressed()[pygame.K_DOWN]):
                 car.ay = car.accel
+
             if (pygame.key.get_pressed()[pygame.K_SPACE]):
                 dirx, diry = car.distanceto(ball, normed = True)
                 ball.ax = dirx
                 ball.ay = diry
             if (pygame.key.get_pressed()[pygame.K_ESCAPE]):
                 gameExit = True
-            if not(pygame.key.get_pressed()[pygame.K_LEFT] or pygame.key.get_pressed()[pygame.K_RIGHT]):
+            if not(pygame.key.get_pressed()[pygame.K_LEFT] or
+                   pygame.key.get_pressed()[pygame.K_RIGHT]):
                 car.ax=0
-            if not(pygame.key.get_pressed()[pygame.K_UP] or pygame.key.get_pressed()[pygame.K_DOWN]):
+            if not(pygame.key.get_pressed()[pygame.K_UP] or
+                   pygame.key.get_pressed()[pygame.K_DOWN]):
                 car.ay=0
             if not(pygame.key.get_pressed()[pygame.K_SPACE]):
                 ball.ax =0
@@ -137,9 +153,13 @@ def game_loop():
         dirx, diry = car.distanceto(enemy)
         enemy.ax = dirx * 0.05
         enemy.ay = diry * 0.05
+        dirx, diry = car.distanceto(enemy2)
+        enemy2.ax = dirx * 0.05
+        enemy2.ay = diry * 0.05
         car.move(display_width, display_height)
         ball.move(display_width, display_height)
         enemy.move(display_width, display_height)
+        enemy2.move(display_width, display_height)
         if car.istouching(enemy):
             if car.deadtime >= 50:
                 print("dead!")
@@ -148,16 +168,25 @@ def game_loop():
             if enemy.deadtime >= 50:
                 print("kill!")
                 enemy.deadtime = 0
+        if car.istouching(enemy2):
+            if car.deadtime >= 50:
+                print("dead!")
+                car.deadtime = 0
+        if ball.istouching(enemy2) and ball.getv() >= 5:
+            if enemy2.deadtime >= 50:
+                print("kill!")
+                enemy2.deadtime = 0
         car.deadtime += 1
         enemy.deadtime += 1
+        enemy2.deadtime += 1
         gameDisplay.fill(white)
         car.draw(gameDisplay)
         ball.draw(gameDisplay)
         enemy.draw(gameDisplay)
+        enemy2.draw(gameDisplay)
 
         pygame.display.update()
         clock.tick(144)
-
 
 
 game_loop()
